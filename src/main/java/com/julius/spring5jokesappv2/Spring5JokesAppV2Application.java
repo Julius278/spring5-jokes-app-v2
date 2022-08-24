d9c38b1c27c730a6a9245fcf8629bccfa5f1a00d
julius278 <julius.lauterbach@t-online.de>
2022-08-24 09:49:01 +0200

    added dependencies and tested the ChuckNorrisQuotes

--- src/main/java/com/julius/spring5jokesappv2/Spring5JokesAppV2Application.java
+++ src/main/java/com/julius/spring5jokesappv2/Spring5JokesAppV2Application.java
@@ -1,5 +1,6 @@
 package com.julius.spring5jokesappv2;
 
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,9 @@ public class Spring5JokesAppV2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Spring5JokesAppV2Application.class, args);
+
+        ChuckNorrisQuotes q = new ChuckNorrisQuotes();
+        System.out.println(q.getRandomQuote());
     }
 
 }
